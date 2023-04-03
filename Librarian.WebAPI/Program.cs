0637e3e3f673be4f92b34f0e3e355a67a54cc1e1
beatriz friso <beatriz.paula@me.com.br>
2023-04-03 20:56:14 -0300

    refactor and add nuget packages

--- Librarian.WebAPI/Program.cs
+++ Librarian.WebAPI/Program.cs
@@ -1,19 +1,25 @@
+using System;
+using Librarian.Infrastructure.EntityFramework;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddEntityFrameworkNpgsql();
+
+builder.Services.AddDbContext<LibraryContext>(opt => opt
+    .UseLazyLoadingProxies().UseInMemoryDatabase("Library"));
+
+builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
